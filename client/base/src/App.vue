52e6436424ae5faac793f5055ab107a5722288b7
KeiTaylor0606 <mvemtae5757@outlook.jp>
2021-11-15 23:24:30 +0900

    captive portal画面の作成

--- client/base/src/App.vue
+++ client/base/src/App.vue
@@ -1,53 +1,36 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="teal lighten-3" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="https://www.tomoyan.net/_media/hardware/gl.inet_logo.svg?w=250&tok=fcb585"
           transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+          width="150"
         />
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn href="https://docs.gl-inet.com/jp/3/" target="_blank" text>
+        <span class="mr-2">Detail</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <HelloWorld />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     HelloWorld,
--- client/base/src/components/HelloWorld.vue
+++ client/base/src/components/HelloWorld.vue
@@ -1,151 +1,136 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+  <div
+    class="bg-body d-flex flex-column justify-center align-center min-vh-100"
+  >
+    <div class="sign-up-card">
+      <div class="sign-up-card-container">
+        <div class="text-center mb-10">
+          <h3 class="mb-3">Authentication</h3>
+          <h5 class="text-sm font-600 grey--text text--darken-4">
+            Authentication will be performed to confirm your identity.
+            <br />Enter the name and login password of your PC's administrator
+            account.
+          </h5>
+        </div>
+        <div class="mb-4">
+          <p class="text-14 mb-1">Username</p>
+          <v-text-field
+            placeholder="Input your account name"
+            v-model="email"
+            outlined
+            dense
+            hide-details=""
+            class="mb-4"
+          ></v-text-field>
+        </div>
+        <div class="mb-4">
+          <p class="text-14 mb-1">Password</p>
+          <v-text-field
+            type="password"
+            v-model="password"
+            placeholder="Input your login pass"
+            outlined
+            dense
+            hide-details=""
+            class="mb-4"
+          ></v-text-field>
+        </div>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
+        <div class="mb-4">
+          <v-btn
+            @click="signin"
+            block
+            color="primary"
+            class="text-capitalize font-600"
           >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+            Start authentication
+          </v-btn>
+        </div>
+        <div
+          class="d-flex align-center justify-center w-200 mx-auto mb-4"
+        ></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+export default {
+  name: "HelloWorld",
 
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
-  }
+  data: () => ({
+    errorMessages: "",
+    name: null,
+    address: null,
+    city: null,
+    state: null,
+    zip: null,
+    country: null,
+    formHasErrors: false,
+  }),
+
+  computed: {
+    form() {
+      return {
+        name: this.name,
+        address: this.address,
+        city: this.city,
+        state: this.state,
+        zip: this.zip,
+        country: this.country,
+      };
+    },
+  },
+
+  watch: {
+    name() {
+      this.errorMessages = "";
+    },
+  },
+
+  methods: {
+    addressCheck() {
+      this.errorMessages =
+        this.address && !this.name ? `Hey! I'm required` : "";
+
+      return true;
+    },
+    resetForm() {
+      this.errorMessages = [];
+      this.formHasErrors = false;
+
+      Object.keys(this.form).forEach((f) => {
+        this.$refs[f].reset();
+      });
+    },
+    submit() {
+      this.formHasErrors = false;
+
+      Object.keys(this.form).forEach((f) => {
+        if (!this.form[f]) this.formHasErrors = true;
+
+        this.$refs[f].validate(true);
+      });
+    },
+  },
+};
 </script>
+<style lang="scss" scoped>
+.sign-up-card {
+  width: 500px;
+  overflow: hidden;
+  background-color: #fff;
+  border-radius: 8px;
+  margin: 2rem auto;
+  box-shadow: rgb(3 0 71 / 9%) 0px 8px 45px;
+  @media (max-width: 500px) {
+    width: 100%;
+  }
+  .sign-up-card-container {
+    padding: 3rem 3.75rem 0px;
+    @media (max-width: 500px) {
+      padding: 3rem 1rem 0px;
+    }
+  }
+}
+</style>
