5726e49206f5239860b0481a435af154aecb07db
KeiTaylor0606 <mvemtae5757@outlook.jp>
2021-11-16 13:27:39 +0900

    階層の変更

---
+++ server/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/labstack/echo"
+)
+
+type UserParams struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/auth", Captive)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func Captive(c echo.Context) error {
+	params := new(UserParams)
+	if err := c.Bind(params); err != nil {
+		return err
+	}
+	if params.Password != "" {
+		fmt.Printf(params.Password)
+		CmdRun(params.Password)
+	}
+	return c.JSON(http.StatusOK, params)
+}
+
+//外部コマンドを実行する関数
+func CmdRun(pass string) {
+	//ルート権限で実行する。
+	cmd := exec.Command("sudo", "-S", "command")
+	cmd.Stdin = strings.NewReader(pass + "\n")
+	cmd.Stdout = os.Stdout
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Err:", err)
+	}
+
+	//ルート証明書を外部コマンドでインストールさせる。
+	cmdMkdir := exec.Command("sudo", "mkdir", "/usr/local/share/ca-certificates/extra")
+	cmdMkdir.Stderr = os.Stderr
+	cmdMkdir.Stdin = os.Stdin
+
+	out, err := cmdMkdir.Output()
+	if err != nil {
+		fmt.Println("Err", err)
+	} else {
+		fmt.Println("mkdir success:", string(out))
+	}
+
+	cmdCp := exec.Command("sudo", "cp", "foo.crt", "/usr/local/share/ca-certificates/extra/foo.crt")
+	cmdCp.Stderr = os.Stderr
+	cmdCp.Stdin = os.Stdin
+
+	out, err = cmdCp.Output()
+	if err != nil {
+		fmt.Println("Err", err)
+	} else {
+		fmt.Println("cp success:", string(out))
+	}
+
+	cmdUpd := exec.Command("sudo", "update-ca-certificates")
+	cmdUpd.Stderr = os.Stderr
+	cmdUpd.Stdin = os.Stdin
+
+	out, err = cmdUpd.Output()
+	if err != nil {
+		fmt.Println("Err", err)
+	} else {
+		fmt.Println("update success:", string(out))
+	}
+}
+
+//http://localhost:1323/auth?username=kimura&password=trapezium
