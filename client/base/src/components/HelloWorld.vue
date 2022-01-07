e5a4135a8fd1fce7276f8936b266e7d844a6ced0
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-07 12:42:55 +0900

    一旦コミット

--- client/base/src/components/HelloWorld.vue
+++ client/base/src/components/HelloWorld.vue
@@ -11,86 +11,43 @@
             max-width="200"
           />
         </div>
+        <div>
+          <v-card
+            class="d-flex flex-column my-6 mx-auto"
+            width="400"
+            color="#fff"
+          >
+            <v-form ref="form" v-model="valid">
+              <v-card-title class="d-flex justify-center pa-0 mt-6 mb-3"
+                >Amazonアカウントでログイン</v-card-title
+              >
+              <v-card-text class="d-flex justify-center flex-column">
+                <div class="mx-9">
+                  <v-text-field
+                    v-model="email"
+                    label="Eメールまたは携帯電話番号"
+                    placeholder="mail@example.com"
+                    outlined
+                    dense
+                    :rules="mailRules"
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="password"
+                    label="パスワード"
+                    placeholder="8文字以上の半角英数記号"
+                    outlined
+                    dense
+                    :rules="pwRules"
+                  ></v-text-field>
+                </div>
+                <div class="my-2">
+                  <v-btn block color="orange lighten-2"> ログイン </v-btn>
+                </div>
+              </v-card-text>
+            </v-form>
+          </v-card>
+        </div>
       </v-col>
-
-      <div>
-        <v-card
-          class="d-flex flex-column my-6 mx-auto"
-          width="400"
-          color="#fff"
-        >
-          <v-form ref="form" v-model="valid">
-            <v-card-title class="d-flex justify-center pa-0 mt-6 mb-3"
-              >Amazonアカウントでログイン</v-card-title
-            >
-            <v-card-text class="d-flex justify-center flex-column">
-              <div class="mx-9">
-                <!-- <v-text-field
-                  label="ユーザー名"
-                  placeholder="15文字以内"
-                  outlined
-                  dense
-                  :rules="nameRules"
-                ></v-text-field> -->
-                <v-text-field
-                  label="Eメールまたは携帯電話番号"
-                  placeholder="mail@example.com"
-                  outlined
-                  dense
-                  :rules="mailRules"
-                ></v-text-field>
-                <v-text-field
-                  label="パスワード"
-                  placeholder="8文字以上の半角英数記号"
-                  outlined
-                  dense
-                  :rules="pwRules"
-                ></v-text-field>
-              </div>
-              <!-- <div class="text-center">
-                <v-btn block :disabled="!valid">ログイン</v-btn>
-              </div> -->
-              <div class="my-2">
-                <v-btn block color="warning"> ログイン </v-btn>
-              </div>
-              <!-- <p class="signUp-border-top text-center mt-6 mb-0 pt-6">
-                ソーシャルアカウントでログイン
-              </p>
-              <v-btn
-                class="fill-width mt-6 text-capitalize caption mx-4"
-                rounded
-                color="#00ACEE"
-                dark
-                depressed
-                height="48px"
-                @click="submitTwitter"
-              >
-                <img
-                  class="button-logo-img mr-4"
-                  src="~/static/twitter.png"
-                  style="height: 20px"
-                />
-                twitterでログイン
-              </v-btn>
-              <v-btn
-                class="fill-width mt-6 text-capitalize caption mx-4 mb-6"
-                rounded
-                height="48px"
-                outlined
-                style="border-color: #979797"
-                @click="submitGoogle"
-              >
-                <img
-                  class="button-logo-img mr-4"
-                  src="https://madeby.google.com/static/images/google_g_logo.svg"
-                  style="height: 24px"
-                />
-                Googleでログイン
-              </v-btn> -->
-            </v-card-text>
-          </v-form>
-        </v-card>
-      </div>
     </v-row>
   </v-container>
 </template>
@@ -110,18 +67,16 @@ export default {
         (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
       ],
       pwRules: [(v) => !!v || "password is required"],
+
+      email: "",
+      password: "",
     };
   },
   methods: {
     validate() {
       this.$refs.form.validate();
     },
-    submitTwitter() {
-      // ツイッターログインの処理
-    },
-    submitGoogle() {
-      // グーグルログインの処理
-    },
+    go() {},
   },
 };
 </script>
