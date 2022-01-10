4fb062d924929c941c36530cf3d38f3398738bac
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-11 00:21:06 +0900

    Main処理(未完)

---
+++ server/server/service/MainService.go
@@ -0,0 +1,10 @@
+package service
+
+func MainService(URL string) error {
+	//1. 叩いたURLの取得
+	//2. ルールに従って、URLを正規のものに戻す
+	//3. 正規URLで正規サーバーにアクセスし、帰ってきたデータをHTMLファイルにして出力
+	//4. ルールに従って、URLを偽物のものに戻す
+	//5. 偽物のURLに変換したデータをHTMLファイルとしてユーザーに返す。
+	return nil
+}
---
+++ server/server/service/MainService_test.go
@@ -0,0 +1,7 @@
+package service
+
+import "testing"
+
+func TestMainService(t *testing.T) {
+
+}
