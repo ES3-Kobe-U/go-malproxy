627df022c9290dbeaa159831ee970dbfec02d7c5
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-11 00:19:38 +0900

    データを読み込み、URLの書き換えを行う

--- server/server/service/ReadDataAndRewriteURL.go
+++ server/server/service/ReadDataAndRewriteURL.go
@@ -5,6 +5,10 @@ import (
 	"strings"
 )
 
+type ReadDataAndRewiteURLService interface {
+	ReadDataAndRewiteURL(fqdn string) (string, error)
+}
+
 func ReadDataAndRewiteURL(fqdn string) (string, error) {
 	data, err := ioutil.ReadFile("test/" + fqdn + ".html") //指定HTMLファイルの読み込み TODO: 後でディレクトリを変更
 	if err != nil {
