ad9285b80211b795610d2faab1257afc30d8cf14
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-11 01:10:43 +0900

    URLからHTMLファイルを取得&自動生成

--- server/server/service/DataExtraction.go
+++ server/server/service/DataExtraction.go
@@ -15,19 +15,26 @@ type DataExtractionService interface {
 }
 
 //URLからHTMLファイルを取得&自動生成
-func DataExtraction(URL string) {
+func DataExtraction(URL string) error {
 	doc, err := goquery.NewDocument(URL)
 	if err != nil {
 		fmt.Print("url scarapping failed")
+		return err
 	}
 	u, err := url.Parse(URL)
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	res, err := doc.Html()
 	if err != nil {
 		fmt.Print("dom get failed")
+		return err
 	}
 	fileName := u.Hostname() //ファイル名はホスト名で統一（多分FQDNの形で返されるので、以後変数名はfqdnで統一したい）
-	ioutil.WriteFile("../../../client/base/src/test/"+fileName+".html", []byte(res), os.ModePerm)
+	err = ioutil.WriteFile("test/"+fileName+".html", []byte(res), os.ModePerm)
+	if err != nil {
+		return err
+	}
+	return nil
 }
