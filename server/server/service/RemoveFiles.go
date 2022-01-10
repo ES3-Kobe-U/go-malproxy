a8368a1f216934f66b8cf9b43d41bce4f49f3ce2
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-11 00:21:50 +0900

    作成したHTMLファイルを削除

--- server/server/service/RemoveFiles.go
+++ server/server/service/RemoveFiles.go
@@ -15,7 +15,6 @@ func RemoveFile(fqdn string) error {
 	cmdRemove := exec.Command("rm", "test/"+fqdn+".html") //指定HTMLファイルの読み込み TODO: 後でディレクトリを変更
 	cmdRemove.Stderr = os.Stderr
 	cmdRemove.Stdin = os.Stdin
-
 	out, err := cmdRemove.Output()
 	if err != nil {
 		log.Println("Err", err)
