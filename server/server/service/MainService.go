1beb195617e3ed7144684729fe2568d017306341
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-11 13:32:08 +0900

    サービス層のメイン部分

--- server/server/service/MainService.go
+++ server/server/service/MainService.go
@@ -1,10 +1,96 @@
 package service
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
 func MainService(URL string) error {
 	//1. 叩いたURLの取得
 	//2. ルールに従って、URLを正規のものに戻す
 	//3. 正規URLで正規サーバーにアクセスし、帰ってきたデータをHTMLファイルにして出力
 	//4. ルールに従って、URLを偽物のものに戻す
 	//5. 偽物のURLに変換したデータをHTMLファイルとしてユーザーに返す。
+	return nil
+}
+
+// Google検索用のURLの作成処理
+func GoogleSearch(Word string) error {
+	var query string
+	cnt := 0
+	for _, char := range Word {
+		if char == ' ' || char == '　' { //空文字列なら+に変換
+			cnt += 1
+			if cnt < 2 {
+				char = '+'
+				query = query + string(char)
+			}
+		} else {
+			cnt = 0
+			query = query + string(char)
+		}
+	}
+	URL := "https://google.com/search?q=" + query //Google検索のURLはこれで統一されているっぽい
+	err := DataExtraction(URL)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ReadDataAndRewiteURL(fqdn string) (string, error) {
+	data, err := ioutil.ReadFile("test/" + fqdn + ".html") //指定HTMLファイルの読み込み TODO: 後でディレクトリを変更
+	if err != nil {
+		return "", err
+	}
+	res := string(data)                                                        //データを文字列に変換
+	rewrite := strings.Replace(res, "https://"+fqdn, "http://go-malproxy", -1) //文字列の置き換え
+	return rewrite, nil
+}
+
+//作成したHTMLファイルを削除する。
+func RemoveFile(fqdn string) error {
+	cmdRemove := exec.Command("rm", "test/"+fqdn+".html") //指定HTMLファイルの読み込み TODO: 後でディレクトリを変更
+	cmdRemove.Stderr = os.Stderr
+	cmdRemove.Stdin = os.Stdin
+	out, err := cmdRemove.Output()
+	if err != nil {
+		log.Println("Err", err)
+		return err
+	} else {
+		log.Println("remove files --> success:", string(out))
+	}
+	return nil
+}
+
+//URLからHTMLファイルを取得&自動生成
+func DataExtraction(URL string) error {
+	doc, err := goquery.NewDocument(URL)
+	if err != nil {
+		fmt.Print("url scarapping failed")
+		return err
+	}
+	u, err := url.Parse(URL)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	res, err := doc.Html()
+	if err != nil {
+		fmt.Print("dom get failed")
+		return err
+	}
+	fileName := u.Hostname() //ファイル名はホスト名で統一（多分FQDNの形で返されるので、以後変数名はfqdnで統一したい）
+	err = ioutil.WriteFile("test/"+fileName+".html", []byte(res), os.ModePerm)
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- server/server/service/MainService_test.go
+++ server/server/service/MainService_test.go
@@ -1,7 +1,128 @@
 package service
 
-import "testing"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"testing"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 func TestMainService(t *testing.T) {
 
+}
+
+var GoogleSearch_Sample1 = []string{
+	"神戸大学",
+	"神戸 大学",
+	"神戸　大学",
+	"KobeUniv.",
+	"Kobe Univ.",
+	"Kobe　Univ.",
+}
+
+var GoogleSearch_Sample2 = []string{
+	"神戸大学",
+	"神戸 大学",
+	"神戸　大学",
+	"KobeUniv.",
+	"Kobe Univ.",
+	"Kobe　Univ.",
+	"神戸大学",
+	"神戸  大学",
+	"神戸　　大学",
+	"KobeUniv.",
+	"Kobe  Univ.",
+	"Kobe　　Univ.",
+}
+
+func TestGoogleSearch(t *testing.T) {
+	for _, word := range GoogleSearch_Sample1 {
+		err := GoogleSearch(word)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func TestMakeURL(t *testing.T) {
+	for _, word := range GoogleSearch_Sample2 {
+		var query string
+		cnt := 0
+		for _, char := range word {
+			if char == ' ' || char == '　' { //空文字列なら+に変換
+				cnt += 1
+				if cnt < 2 {
+					char = '+'
+					query = query + string(char)
+				}
+			} else {
+				cnt = 0
+				query = query + string(char)
+			}
+		}
+		URL := "https://google.com/search?q=" + query //Google検索のURLはこれで統一されているっぽい
+		fmt.Println(URL)
+	}
+}
+
+var ReadData_Sample = []string{
+	"www.google.com",
+	"www.amazon.co.jp",
+}
+
+func TestReadData(t *testing.T) {
+	for _, fqdn := range ReadData_Sample {
+		res, err := ReadDataAndRewiteURL(fqdn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ioutil.WriteFile("test/rewite_"+fqdn+".html", []byte(res), os.ModePerm)
+	}
+}
+
+var RemoveFile_Sample1 = []string{
+	"https://www.google.com/",
+	"https://www.amazon.co.jp/-/en/",
+}
+
+func TestRemoveFile(t *testing.T) {
+	for _, Url := range RemoveFile_Sample1 {
+		u, err := url.Parse(Url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = RemoveFile(u.Hostname())
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+var DataExtraction_Sample = []string{
+	"https://www.google.com/",
+	"https://www.amazon.co.jp/-/en/",
+	"https://www.google.com/search?q=Golang",
+}
+
+func TestDataExtraction(t *testing.T) {
+	for _, Url := range DataExtraction_Sample {
+		doc, err := goquery.NewDocument(Url)
+		if err != nil {
+			fmt.Print("url scarapping failed")
+		}
+		u, err := url.Parse(Url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		res, err := doc.Html()
+		if err != nil {
+			fmt.Print("dom get failed")
+		}
+		fileName := u.Hostname()
+		ioutil.WriteFile("test/"+fileName+".html", []byte(res), os.ModePerm)
+	}
 }
