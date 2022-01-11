e9b01c02831296139a57873040c6df033a1774df
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-11 13:33:20 +0900

    Google検索

--- server/server/service/GoogleSearch.go
+++ server/server/service/GoogleSearch.go
@@ -1,25 +1,25 @@
 package service
 
 // Google検索用のURLの作成処理
-func GoogleSearch(Word string) error {
-	var query string
-	cnt := 0
-	for _, char := range Word {
-		if char == ' ' || char == '　' { //空文字列なら+に変換
-			cnt += 1
-			if cnt < 2 {
-				char = '+'
-				query = query + string(char)
-			}
-		} else {
-			cnt = 0
-			query = query + string(char)
-		}
-	}
-	URL := "https://google.com/search?q=" + query //Google検索のURLはこれで統一されているっぽい
-	err := DataExtraction(URL)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// func GoogleSearch(Word string) error {
+// 	var query string
+// 	cnt := 0
+// 	for _, char := range Word {
+// 		if char == ' ' || char == '　' { //空文字列なら+に変換
+// 			cnt += 1
+// 			if cnt < 2 {
+// 				char = '+'
+// 				query = query + string(char)
+// 			}
+// 		} else {
+// 			cnt = 0
+// 			query = query + string(char)
+// 		}
+// 	}
+// 	URL := "https://google.com/search?q=" + query //Google検索のURLはこれで統一されているっぽい
+// 	err := DataExtraction(URL)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
--- server/server/service/GoogleSearch_test.go
+++ server/server/service/GoogleSearch_test.go
@@ -1,61 +1,55 @@
 package service
 
-import (
-	"fmt"
-	"log"
-	"testing"
-)
+// var GoogleSearch_Sample1 = []string{
+// 	"神戸大学",
+// 	"神戸 大学",
+// 	"神戸　大学",
+// 	"KobeUniv.",
+// 	"Kobe Univ.",
+// 	"Kobe　Univ.",
+// }
 
-var WordList_Sample1 = []string{
-	"神戸大学",
-	"神戸 大学",
-	"神戸　大学",
-	"KobeUniv.",
-	"Kobe Univ.",
-	"Kobe　Univ.",
-}
+// var GoogleSearch_Sample2 = []string{
+// 	"神戸大学",
+// 	"神戸 大学",
+// 	"神戸　大学",
+// 	"KobeUniv.",
+// 	"Kobe Univ.",
+// 	"Kobe　Univ.",
+// 	"神戸大学",
+// 	"神戸  大学",
+// 	"神戸　　大学",
+// 	"KobeUniv.",
+// 	"Kobe  Univ.",
+// 	"Kobe　　Univ.",
+// }
 
-var WordList_Sample2 = []string{
-	"神戸大学",
-	"神戸 大学",
-	"神戸　大学",
-	"KobeUniv.",
-	"Kobe Univ.",
-	"Kobe　Univ.",
-	"神戸大学",
-	"神戸  大学",
-	"神戸　　大学",
-	"KobeUniv.",
-	"Kobe  Univ.",
-	"Kobe　　Univ.",
-}
+// func TestGoogleSearch(t *testing.T) {
+// 	for _, word := range GoogleSearch_Sample1 {
+// 		err := GoogleSearch(word)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}
+// }
 
-func TestMakeURL(t *testing.T) {
-	for _, word := range WordList_Sample2 {
-		var query string
-		cnt := 0
-		for _, char := range word {
-			if char == ' ' || char == '　' { //空文字列なら+に変換
-				cnt += 1
-				if cnt < 2 {
-					char = '+'
-					query = query + string(char)
-				}
-			} else {
-				cnt = 0
-				query = query + string(char)
-			}
-		}
-		URL := "https://google.com/search?q=" + query //Google検索のURLはこれで統一されているっぽい
-		fmt.Println(URL)
-	}
-}
-
-func TestGoogleSearch(t *testing.T) {
-	for _, word := range WordList_Sample1 {
-		err := GoogleSearch(word)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
+// func TestMakeURL(t *testing.T) {
+// 	for _, word := range GoogleSearch_Sample2 {
+// 		var query string
+// 		cnt := 0
+// 		for _, char := range word {
+// 			if char == ' ' || char == '　' { //空文字列なら+に変換
+// 				cnt += 1
+// 				if cnt < 2 {
+// 					char = '+'
+// 					query = query + string(char)
+// 				}
+// 			} else {
+// 				cnt = 0
+// 				query = query + string(char)
+// 			}
+// 		}
+// 		URL := "https://google.com/search?q=" + query //Google検索のURLはこれで統一されているっぽい
+// 		fmt.Println(URL)
+// 	}
+// }
