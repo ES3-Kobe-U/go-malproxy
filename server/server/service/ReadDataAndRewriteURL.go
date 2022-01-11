1ac6f4979da105e6a95d894c1f8c579c1885447d
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-11 13:34:10 +0900

    データの読み込みとURLの書き換え

--- server/server/service/ReadDataAndRewriteURL.go
+++ server/server/service/ReadDataAndRewriteURL.go
@@ -1,20 +1,15 @@
 package service
 
-import (
-	"io/ioutil"
-	"strings"
-)
+// type ReadDataAndRewiteURLService interface {
+// 	ReadDataAndRewiteURL(fqdn string) (string, error)
+// }
 
-type ReadDataAndRewiteURLService interface {
-	ReadDataAndRewiteURL(fqdn string) (string, error)
-}
-
-func ReadDataAndRewiteURL(fqdn string) (string, error) {
-	data, err := ioutil.ReadFile("test/" + fqdn + ".html") //指定HTMLファイルの読み込み TODO: 後でディレクトリを変更
-	if err != nil {
-		return "", err
-	}
-	res := string(data)                                                        //データを文字列に変換
-	rewrite := strings.Replace(res, "https://"+fqdn, "http://go-malproxy", -1) //文字列の置き換え
-	return rewrite, nil
-}
+// func ReadDataAndRewiteURL(fqdn string) (string, error) {
+// 	data, err := ioutil.ReadFile("test/" + fqdn + ".html") //指定HTMLファイルの読み込み TODO: 後でディレクトリを変更
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	res := string(data)                                                        //データを文字列に変換
+// 	rewrite := strings.Replace(res, "https://"+fqdn, "http://go-malproxy", -1) //文字列の置き換え
+// 	return rewrite, nil
+// }
--- server/server/service/ReadDataAndRewriteURL_test.go
+++ server/server/service/ReadDataAndRewriteURL_test.go
@@ -1,23 +1,16 @@
 package service
 
-import (
-	"io/ioutil"
-	"log"
-	"os"
-	"testing"
-)
+// var ReadData_Sample = []string{
+// 	"www.google.com",
+// 	"www.amazon.co.jp",
+// }
 
-var FQDN = map[int]string{
-	1: "www.google.com",
-	2: "www.amazon.co.jp",
-}
-
-func TestReadData(t *testing.T) {
-	for _, fqdn := range FQDN {
-		res, err := ReadDataAndRewiteURL(fqdn)
-		if err != nil {
-			log.Fatal(err)
-		}
-		ioutil.WriteFile("test/rewite_"+fqdn+".html", []byte(res), os.ModePerm)
-	}
-}
+// func TestReadData(t *testing.T) {
+// 	for _, fqdn := range ReadData_Sample {
+// 		res, err := ReadDataAndRewiteURL(fqdn)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		ioutil.WriteFile("test/rewite_"+fqdn+".html", []byte(res), os.ModePerm)
+// 	}
+// }
