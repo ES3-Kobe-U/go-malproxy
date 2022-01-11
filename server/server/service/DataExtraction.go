cd1db2ba9912443d1625594f81b87df6e17e537b
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-11 13:32:53 +0900

    データ抽出

--- server/server/service/DataExtraction.go
+++ server/server/service/DataExtraction.go
@@ -1,40 +1,30 @@
 package service
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/url"
-	"os"
+// type DataExtractionService interface {
+// 	DataExtraction(URL string)
+// }
 
-	"github.com/PuerkitoBio/goquery"
-)
-
-type DataExtractionService interface {
-	DataExtraction(URL string)
-}
-
-//URLからHTMLファイルを取得&自動生成
-func DataExtraction(URL string) error {
-	doc, err := goquery.NewDocument(URL)
-	if err != nil {
-		fmt.Print("url scarapping failed")
-		return err
-	}
-	u, err := url.Parse(URL)
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
-	res, err := doc.Html()
-	if err != nil {
-		fmt.Print("dom get failed")
-		return err
-	}
-	fileName := u.Hostname() //ファイル名はホスト名で統一（多分FQDNの形で返されるので、以後変数名はfqdnで統一したい）
-	err = ioutil.WriteFile("test/"+fileName+".html", []byte(res), os.ModePerm)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// //URLからHTMLファイルを取得&自動生成
+// func DataExtraction(URL string) error {
+// 	doc, err := goquery.NewDocument(URL)
+// 	if err != nil {
+// 		fmt.Print("url scarapping failed")
+// 		return err
+// 	}
+// 	u, err := url.Parse(URL)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 		return err
+// 	}
+// 	res, err := doc.Html()
+// 	if err != nil {
+// 		fmt.Print("dom get failed")
+// 		return err
+// 	}
+// 	fileName := u.Hostname() //ファイル名はホスト名で統一（多分FQDNの形で返されるので、以後変数名はfqdnで統一したい）
+// 	err = ioutil.WriteFile("test/"+fileName+".html", []byte(res), os.ModePerm)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
--- server/server/service/DataExtraction_test.go
+++ server/server/service/DataExtraction_test.go
@@ -1,37 +1,26 @@
 package service
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/url"
-	"os"
-	"testing"
+// var DataExtraction_Sample = []string{
+// 	"https://www.google.com/",
+// 	"https://www.amazon.co.jp/-/en/",
+// 	"https://www.google.com/search?q=Golang",
+// }
 
-	"github.com/PuerkitoBio/goquery"
-)
-
-var URL = map[int]string{
-	1: "https://www.google.com/",
-	2: "https://www.amazon.co.jp/-/en/",
-	3: "https://www.google.com/search?q=Golang",
-}
-
-func TestDataExtraction(t *testing.T) {
-	for _, Url := range URL {
-		doc, err := goquery.NewDocument(Url)
-		if err != nil {
-			fmt.Print("url scarapping failed")
-		}
-		u, err := url.Parse(Url)
-		if err != nil {
-			log.Fatal(err)
-		}
-		res, err := doc.Html()
-		if err != nil {
-			fmt.Print("dom get failed")
-		}
-		fileName := u.Hostname()
-		ioutil.WriteFile("test/"+fileName+".html", []byte(res), os.ModePerm)
-	}
-}
+// func TestDataExtraction(t *testing.T) {
+// 	for _, Url := range DataExtraction_Sample {
+// 		doc, err := goquery.NewDocument(Url)
+// 		if err != nil {
+// 			fmt.Print("url scarapping failed")
+// 		}
+// 		u, err := url.Parse(Url)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		res, err := doc.Html()
+// 		if err != nil {
+// 			fmt.Print("dom get failed")
+// 		}
+// 		fileName := u.Hostname()
+// 		ioutil.WriteFile("test/"+fileName+".html", []byte(res), os.ModePerm)
+// 	}
+// }
