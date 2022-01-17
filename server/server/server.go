6af43bcd2e198cc77cb06c19414f0f158483faee
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-17 19:01:35 +0900

    serverの再定義

--- server/server/server.go
+++ server/server/server.go
@@ -1,16 +1,16 @@
 package server
 
 import (
-	"github.com/go-malproxy/server/handler"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 func Run() {
 	e := echo.New()
+	e.Renderer = t
 	e.Use(middleware.CORS())
-	//Router
-	e.GET("/login", handler.AssignPostLoginData)
-	e.GET("google-search/", handler.AssignGoogleSearchHandler)
+	InitRouter(e)
 	e.Logger.Fatal(e.Start(":1323"))
 }
+
+//http://localhost:1323/
