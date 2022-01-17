edab7e405e8366aaf8d9942981234a1528351959
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-17 19:01:06 +0900

    routerの再定義

--- server/server/router.go
+++ server/server/router.go
@@ -1,11 +1,27 @@
 package server
 
 import (
+	"io"
+	"text/template"
+
 	"github.com/go-malproxy/server/handler"
 	"github.com/labstack/echo"
 )
 
-func Router(e *echo.Echo) {
-	e.POST("api/v1/login", handler.AssignPostLoginData)
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.Execute(w, nil)
+}
+
+var t = &Template{
+	templates: template.Must(template.ParseGlob("/home/kimura/go-malproxy/server/server/views/*.html")),
+}
+
+func InitRouter(e *echo.Echo) {
+	e.GET("/", handler.AssignMainHandler)
+	e.GET("/login", handler.AssignPostLoginData)
 	e.GET("google-search/", handler.AssignGoogleSearchHandler)
 }
