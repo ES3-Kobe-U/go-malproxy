ba9b0a380c833d328f3ed557f54c401f20fc4982
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-24 17:16:18 +0900

    Google検索

--- server/service/GoogleSearch.go
+++ server/service/GoogleSearch.go
@@ -1,25 +1,29 @@
 package service
 
-// Google検索用のURLの作成処理
-// func GoogleSearch(Word string) error {
-// 	var query string
-// 	cnt := 0
-// 	for _, char := range Word {
-// 		if char == ' ' || char == '　' { //空文字列なら+に変換
-// 			cnt += 1
-// 			if cnt < 2 {
-// 				char = '+'
-// 				query = query + string(char)
-// 			}
-// 		} else {
-// 			cnt = 0
-// 			query = query + string(char)
-// 		}
-// 	}
-// 	URL := "https://google.com/search?q=" + query //Google検索のURLはこれで統一されているっぽい
-// 	err := DataExtraction(URL)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	return nil
-// }
+/*
+GoogleSearch関数
+
+検索ワードを引数にとって、その検索結果のhtmlファイルを自動生成する。
+*/
+func GoogleSearch(Word string) error {
+	var query string
+	cnt := 0
+	for _, char := range Word {
+		if char == ' ' || char == '　' { //空文字列なら+に変換
+			cnt += 1
+			if cnt < 2 {
+				char = '+'
+				query = query + string(char)
+			}
+		} else {
+			cnt = 0
+			query = query + string(char)
+		}
+	}
+	URL := "https://google.com/search?q=" + query //Google検索のURLはこれで統一されているっぽい
+	err := DataExtraction(URL)
+	if err != nil {
+		return err
+	}
+	return nil
+}
