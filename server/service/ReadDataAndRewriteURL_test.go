44ae7ca7e5d7de147b613a584aed10bbdf29baf7
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-24 16:13:37 +0900

    service

---
+++ server/service/ReadDataAndRewriteURL_test.go
@@ -0,0 +1,16 @@
+package service
+
+// var ReadData_Sample = []string{
+// 	"www.google.com",
+// 	"www.amazon.co.jp",
+// }
+
+// func TestReadData(t *testing.T) {
+// 	for _, fqdn := range ReadData_Sample {
+// 		res, err := ReadDataAndRewiteURL(fqdn)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		ioutil.WriteFile("test/rewite_"+fqdn+".html", []byte(res), os.ModePerm)
+// 	}
+// }
---
+++ server/service/RemoveFile_test.go
@@ -0,0 +1,19 @@
+package service
+
+// var RemoveFile_Sample1 = []string{
+// 	"https://www.google.com/",
+// 	"https://www.amazon.co.jp/-/en/",
+// }
+
+// func TestRemoveFile(t *testing.T) {
+// 	for _, Url := range RemoveFile_Sample1 {
+// 		u, err := url.Parse(Url)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		err = RemoveFile(u.Hostname())
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}
+// }
