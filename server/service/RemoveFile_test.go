025f6d143d9e50923aa70b002dcc3539b4e87b0b
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-01-24 17:37:31 +0900

    不要ファイル削除

--- server/service/RemoveFile_test.go
+++ server/service/RemoveFile_test.go
@@ -1,19 +1,25 @@
 package service
 
-// var RemoveFile_Sample1 = []string{
-// 	"https://www.google.com/",
-// 	"https://www.amazon.co.jp/-/en/",
-// }
+import (
+	"log"
+	"net/url"
+	"testing"
+)
 
-// func TestRemoveFile(t *testing.T) {
-// 	for _, Url := range RemoveFile_Sample1 {
-// 		u, err := url.Parse(Url)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		err = RemoveFile(u.Hostname())
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 	}
-// }
+var RemoveList = []string{
+	"https://www.google.com/",
+	"https://www.amazon.co.jp/-/en/",
+}
+
+func TestRemoveFile(t *testing.T) {
+	for _, Url := range RemoveList {
+		u, err := url.Parse(Url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = RemoveFile(u.Hostname())
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
--- server/service/RemoveFiles.go
+++ server/service/RemoveFiles.go
@@ -1,20 +1,30 @@
 package service
 
-// type RemoveFileService interface {
-// 	RemoveFile(fqdn string) error
-// }
+import (
+	"log"
+	"os"
+	"os/exec"
+)
 
-// //作成したHTMLファイルを削除する。
-// func RemoveFile(fqdn string) error {
-// 	cmdRemove := exec.Command("rm", "test/"+fqdn+".html") //指定HTMLファイルの読み込み TODO: 後でディレクトリを変更
-// 	cmdRemove.Stderr = os.Stderr
-// 	cmdRemove.Stdin = os.Stdin
-// 	out, err := cmdRemove.Output()
-// 	if err != nil {
-// 		log.Println("Err", err)
-// 		return err
-// 	} else {
-// 		log.Println("remove files --> success:", string(out))
-// 	}
-// 	return nil
-// }
+type RemoveFileService interface {
+	RemoveFile(fqdn string) error
+}
+
+/*
+RemoveFile関数
+
+FQDNを引数にとって、FQDN.htmlを外部コマンドで削除する。
+*/
+func RemoveFile(fqdn string) error {
+	cmdRemove := exec.Command("rm", "test/"+fqdn+".html") //指定HTMLファイルの読み込み TODO: 後でディレクトリを変更
+	cmdRemove.Stderr = os.Stderr
+	cmdRemove.Stdin = os.Stdin
+	out, err := cmdRemove.Output()
+	if err != nil {
+		log.Println("Err", err)
+		return err
+	} else {
+		log.Println("remove files --> success:", string(out))
+	}
+	return nil
+}
