1c233d8685ceec02db32da82bab17192bddf46f1
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-02-10 14:45:08 +0900

    入力情報と登録情報の整合性の確認

--- server/service/CheckingConsistency.go
+++ server/service/CheckingConsistency.go
@@ -3,7 +3,6 @@ package service
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"time"
 
@@ -17,68 +16,103 @@ import (
 Amazon用の処理
 */
 func CheckingTheIntegrityOfAmazonInformation(email string, password string) error {
+	var res0 []byte
 	var res1 []byte
 	var res2 []byte
-	var data string
-	var str string = "xxxxxx"
+	var res3 []byte
+	// var data string
+	// var str string = "xxxxxx"
 	ctx, cancel := chromedp.NewContext(context.Background(), chromedp.WithBrowserOption())
 	defer cancel()
-	err := chromedp.Run(ctx,
+
+	task1 := chromedp.Tasks{ //タスクリストの作成
 		chromedp.Navigate("https://www.amazon.co.jp/stores/page/FD12D065-F94A-4656-B816-4C8121739FAE?ingress=3"),
 		chromedp.WaitReady("body"),
+		chromedp.CaptureScreenshot(&res0),
 		chromedp.Click(`a[data-nav-role='signin']`, chromedp.ByQuery),
-		chromedp.Sleep(time.Second*2),
+		chromedp.Sleep(time.Second * 2),
 		chromedp.SetValue(`ap_email`, email, chromedp.ByID),
 		chromedp.Click(`continue`, chromedp.ByID),
-		chromedp.Sleep(time.Second*1),
+		chromedp.Sleep(time.Second * 1),
 		chromedp.SetValue(`ap_password`, password, chromedp.ByID),
-		chromedp.Click(`signInSubmit`, chromedp.ByID),
-		chromedp.Sleep(time.Second*2),
 		chromedp.CaptureScreenshot(&res1),
-		chromedp.SetValue(`ap_password`, password, chromedp.ByID),
-		chromedp.ActionFunc(func(c context.Context) error {
-			fmt.Println("output --> res1.png")
-			err := os.WriteFile("./res1.png", res1, 0644)
-			if err != nil {
-				return err
-			}
-			return nil
-		}),
-		chromedp.Sleep(time.Second*10),
-		chromedp.ActionFunc(func(c context.Context) error {
-			var input string
-			fmt.Print("fmt.Scan >")
-			fmt.Scanf("%s", &input)
-			str = input
-			fmt.Println("input:", str)
-			return nil
-		}),
-		chromedp.Sleep(time.Second*10),
-		chromedp.SetValue(`auth-captcha-guess`, str, chromedp.ByID),
+		chromedp.Click(`signInSubmit`, chromedp.ByID),
+		chromedp.Sleep(time.Second * 2),
 		chromedp.CaptureScreenshot(&res2),
-		chromedp.ScrollIntoView(`footer`),
-		chromedp.Text(`h1`, &data, chromedp.NodeVisible, chromedp.ByQuery),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			node, err := dom.GetDocument().Do(ctx)
-			if err != nil {
-				return err
-			}
-			data, err := dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)
-			if err != nil {
-				return err
-			}
-			err = ioutil.WriteFile("result.html", []byte(data), os.ModePerm)
-			if err != nil {
-				return err
-			}
-			return nil
-		}),
-	)
+	}
+
+	err := chromedp.Run(ctx, task1)
 	if err != nil {
 		return err
 	}
-	//os.WriteFile("./res1.png", res1, 0644)
+
+	task2 := chromedp.Tasks{
+		chromedp.CaptureScreenshot(&res3),
+	}
+
+	err = chromedp.Run(ctx, task2) //セッションが維持されているかの確認
+	if err != nil {
+		return err
+	}
+
+	// err := chromedp.Run(ctx,
+	// 	chromedp.Navigate("https://www.amazon.co.jp/stores/page/FD12D065-F94A-4656-B816-4C8121739FAE?ingress=3"),
+	// 	chromedp.WaitReady("body"),
+	// 	chromedp.Click(`a[data-nav-role='signin']`, chromedp.ByQuery),
+	// 	chromedp.Sleep(time.Second*2),
+	// 	chromedp.SetValue(`ap_email`, email, chromedp.ByID),
+	// 	chromedp.Click(`continue`, chromedp.ByID),
+	// 	chromedp.Sleep(time.Second*1),
+	// 	chromedp.SetValue(`ap_password`, password, chromedp.ByID),
+	// 	chromedp.Click(`signInSubmit`, chromedp.ByID),
+	// 	chromedp.Sleep(time.Second*2),
+	// 	chromedp.CaptureScreenshot(&res1),
+	// 	chromedp.SetValue(`ap_password`, password, chromedp.ByID),
+	// 	chromedp.ActionFunc(func(c context.Context) error {
+	// 		fmt.Println("output --> res1.png")
+	// 		err := os.WriteFile("./res1.png", res1, 0644)
+	// 		if err != nil {
+	// 			return err
+	// 		}
+	// 		return nil
+	// 	}),
+	// 	chromedp.Sleep(time.Second*10),
+	// 	chromedp.ActionFunc(func(c context.Context) error {
+	// 		var input string
+	// 		fmt.Print("fmt.Scan >")
+	// 		fmt.Scanf("%s", &input)
+	// 		str = input
+	// 		fmt.Println("input:", str)
+	// 		return nil
+	// 	}),
+	// 	chromedp.Sleep(time.Second*10),
+	// 	chromedp.SetValue(`auth-captcha-guess`, str, chromedp.ByID),
+	// 	chromedp.CaptureScreenshot(&res2),
+	// 	chromedp.ScrollIntoView(`footer`),
+	// 	chromedp.Text(`h1`, &data, chromedp.NodeVisible, chromedp.ByQuery),
+	// 	chromedp.ActionFunc(func(ctx context.Context) error {
+	// 		node, err := dom.GetDocument().Do(ctx)
+	// 		if err != nil {
+	// 			return err
+	// 		}
+	// 		data, err := dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)
+	// 		if err != nil {
+	// 			return err
+	// 		}
+	// 		err = ioutil.WriteFile("result.html", []byte(data), os.ModePerm)
+	// 		if err != nil {
+	// 			return err
+	// 		}
+	// 		return nil
+	// 	}),
+	// )
+	// if err != nil {
+	// 	return err
+	// }
+	os.WriteFile("./res0.png", res0, 0644)
+	os.WriteFile("./res1.png", res1, 0644)
 	os.WriteFile("./res2.png", res2, 0644)
+	os.WriteFile("./res3.png", res3, 0644)
 	return nil
 }
 
--- server/service/CheckingConsistency_test.go
+++ server/service/CheckingConsistency_test.go
@@ -5,28 +5,28 @@ import (
 	"testing"
 )
 
-// func TestCheckingTheIntegrityOfAmazonInformation(t *testing.T) {
-// 	testcases := []struct {
-// 		name     string
-// 		email    string
-// 		password string
-// 	}{
-// 		//各々テストケースを記述
-// 		{
-// 			name:     "example-00",
-// 			email:    `example.00@email.com`,
-// 			password: `example00`,
-// 		},
-// 	}
+func TestCheckingTheIntegrityOfAmazonInformation(t *testing.T) {
+	testcases := []struct {
+		name     string
+		email    string
+		password string
+	}{
+		//各々テストケースを記述
+		{
+			name:     "example-00",
+			email:    `example.00@email.com`,
+			password: `example00`,
+		},
+	}
 
-// 	for _, tetestcase := range testcases {
-// 		t.Run(tetestcase.name, func(t *testing.T) {
-// 			if err := CheckingTheIntegrityOfAmazonInformation(tetestcase.email, tetestcase.password); err != nil {
-// 				log.Fatal(err)
-// 			}
-// 		})
-// 	}
-// }
+	for _, tetestcase := range testcases {
+		t.Run(tetestcase.name, func(t *testing.T) {
+			if err := CheckingTheIntegrityOfAmazonInformation(tetestcase.email, tetestcase.password); err != nil {
+				log.Fatal(err)
+			}
+		})
+	}
+}
 
 func TestCheckingTheIntegrityOfRakutenInformation(t *testing.T) {
 	testcases := []struct {
