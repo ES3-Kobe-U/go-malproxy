aa54bc828ef8301c02749cf9f8a9945b68210ed2
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-03-10 03:21:21 +0900

    Main操作

--- server/service/main_operation.go
+++ server/service/main_operation.go
@@ -66,9 +66,9 @@ func ReadDataAndRewiteURL(fqdn string) error {
 	if err != nil {
 		return err
 	}
-	rew := strings.Replace(res, `<a href="`, `<a href="http://localhost:3333/template?url=`, -1) //文字列の置き換え
-	rew = strings.Replace(rew, `<a href='`, `<a href='http://localhost:3333/template?url=`, -1)  //文字列の置き換え
-	rew = strings.Replace(rew, `pa3.min.js`, ``, -1)                                             //楽天のCORSを回避する為に削除
+	rew := strings.Replace(res, `<a href="`, `<a href="/template?url=`, -1) //文字列の置き換え
+	rew = strings.Replace(rew, `<a href='`, `<a href='/template?url=`, -1)  //文字列の置き換え
+	rew = strings.Replace(rew, `pa3.min.js`, ``, -1)                        //楽天のCORSを回避する為に削除
 	rew = strings.Replace(rew, `<div
     irc="MembershipHeader"
     data-show-new="true"
