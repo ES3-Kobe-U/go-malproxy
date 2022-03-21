e5cfb66a9df13b8e34608e8d947318ce23dcad3b
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-03-21 16:29:50 +0000

    interfac

--- server/service/servises.go
+++ server/service/servises.go
@@ -1,8 +1,10 @@
 package service
 
+import "context"
+
 type Service interface {
-	CheckingTheIntegrityOfAmazonInformation(email string, password string) error
-	CheckingTheIntegrityOfRakutenInformation(userId string, password string) error
-	CheckingContextContents() error
-	CheckingTheIntegrityOfAmazonCaptcha(password string, guess string) error
+	CheckingTheIntegrityOfAmazonInformation(ctx context.Context, email string, password string) (context.Context, error)
+	CheckingTheIntegrityOfRakutenInformation(ctx context.Context, userId string, password string) (context.Context, error)
+	CheckingContextContents(ctx context.Context) error
+	CheckingTheIntegrityOfAmazonCaptcha(ctx context.Context, password string, guess string) (context.Context, error)
 }
